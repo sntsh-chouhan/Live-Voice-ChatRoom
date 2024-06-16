3916607532cea813c93e179b0673f9bd3e6bc6e9
santosh <santoshchouhan2442@gmail.com>
2024-06-16 15:08:44 +0530

    activation, setName setAvatar

--- frontend/src/pages/Steps/StepName/StepName.jsx
+++ frontend/src/pages/Steps/StepName/StepName.jsx
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Card from '../../../components/shared/Card/Card';
+import Button from '../../../components/shared/Button/Button';
+import TextInput from '../../../components/shared/TextInput/TextInput';
+import { useDispatch, useSelector } from 'react-redux';
+import { setName } from '../../../store/activateSlice';
+import styles from './StepName.module.css';
+const StepName = ({ onNext }) => {
 
-const StepName = ({onNext}) => {
-  return (
-    <>
-      <div>
-        name dalo bhai 
-      </div>
-      <button onClick={onNext}>next</button>
-    </>
-  );
+    const { name } = useSelector((state) => state.activate);
+    const dispatch = useDispatch();
+
+    const [fullname, setFullname] = useState(name);
+
+    function nextStep() {
+        if (!fullname) {
+            return;
+        }
+        dispatch(setName(fullname));
+        onNext();
+    }
+    
+    return (
+        <>
+            <Card title="What’s your full name?" icon="goggle-emoji">
+                <TextInput
+                    value={fullname}
+                    onChange={(e) => setFullname(e.target.value)}
+                />
+                <p className={styles.paragraph}>
+                    People use real names at codershouse :) !
+                </p>
+                <div>
+                    <Button image="arow" writeup="Next" onClick={nextStep} />
+                </div>
+            </Card>
+        </>
+    );
 };
 
 export default StepName;
