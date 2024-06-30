a10f7db9d7ac19f9091f19267847ca4ee859d97c
santosh <santoshchouhan2442@gmail.com>
2024-06-30 16:25:28 +0530

    hook to keep logged-in on refresh

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,34 @@
-import './App.css';
-import {BrowserRouter, Switch, Route, Redirect} from 'react-router-dom';
-import Home from './pages/Home/Home';
-import Navigation from './components/shared/Navigation/Navigation';
-import Authenticate from './pages/Authenticate/Authenticate';
-import Activate from './pages/Activate/Activate';
-import Rooms from './pages/Rooms/Rooms';
 import { useSelector } from 'react-redux';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import './App.css';
+import Navigation from './components/shared/Navigation/Navigation';
+import Activate from './pages/Activate/Activate';
+import Authenticate from './pages/Authenticate/Authenticate';
+import Home from './pages/Home/Home';
+import Rooms from './pages/Rooms/Rooms';
+import { useLoadingWithRefresh } from './hooks/useLoadingWithRefresh';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Navigation />
-      <Switch>
-        <GuestRoute path= "/" exact>
-          <Home/>
-        </GuestRoute>
-        <GuestRoute path="/authenticate">
-          <Authenticate />
-        </GuestRoute>
-        <SemiProtectedRoute path='/activate'>
-          <Activate/>
-        </SemiProtectedRoute>
-        <ProtectedRoute path='/rooms'>
-          <Rooms />
-        </ProtectedRoute>
-        
-      </Switch>
-    </BrowserRouter>
-  );
+    const {loading} = useLoadingWithRefresh();
+    return loading? ('loading...') : (
+        <BrowserRouter>
+        <Navigation />
+        <Switch>
+            <GuestRoute path= "/" exact>
+                <Home/>
+            </GuestRoute>
+            <GuestRoute path="/authenticate">
+                <Authenticate />
+            </GuestRoute>
+            <SemiProtectedRoute path='/activate'>
+                <Activate/>
+            </SemiProtectedRoute>
+            <ProtectedRoute path='/rooms'>
+                <Rooms />
+            </ProtectedRoute>
+        </Switch>
+        </BrowserRouter>
+    );
 }
 
 const GuestRoute =({children, ...rest}) =>{
